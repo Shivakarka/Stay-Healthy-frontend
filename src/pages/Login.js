2ea812551331265605876f61eb7d870d1202c46c
shivakarka <shivakarka@gmail.com>
2022-11-11 18:22:35 +0530

    designed login/register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Button, Form, Image, Input } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./doc.svg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const onFinish = (values) => {
+    console.log("Received ", values);
+  };
 
-export default Login
+  return (
+    <div className="authentication">
+      <div className="logo">
+        <img src={Logo} />
+      </div>
+      <div className="authentication-form card p-3">
+        <h1 style={{ textAlign: "center" }}>STAY HEALTHY</h1>
+        <h1 className="card-title">Login Here</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+
+          <Button
+            className="primary-button my-2 full-width-button"
+            htmlType="submit"
+          >
+            Login
+          </Button>
+
+          <Link to="/register" className="anchor mt-3">
+            CLICK HERE TO REGISTER
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,42 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const onFinish = (values) => {
+    console.log("Received ", values);
+  };
+
+  return (
+    <div className="authentication">
+      <div className="authentication-form card p-3">
+        <h1 style={{ textAlign: "center" }}>STAY HEALTHY</h1>
+        <h1 className="card-title">Nice To Meet U</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Name" name="name">
+            <Input placeholder="Name" />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+
+          <Button
+            className="primary-button my-2 full-width-button"
+            htmlType="submit"
+          >
+            REGISTER
+          </Button>
+
+          <Link to="/login" className="anchor mt-3">
+            CLICK HERE TO LOGIN
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
